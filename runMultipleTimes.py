01712de02f7ffb5a16d7e9728f802a1b73d1d287
atefahmed <atefa2009@gmail.com>
2018-03-12 16:41:13 +0200

    version 1.13

--- runMultipleTimes.py
+++ runMultipleTimes.py
@@ -1,7 +1,7 @@
 
 
-for i in range(200):
-    exec(open("./generateTextBasedOnLineSeedDescription.py").read())
+# for i in range(200):
+#     exec(open("./generateTextBasedOnLineSeedDescription.py").read())
 
 # for i in range(200):
 #     exec(open("./generateTextBasedOnLineSeedTitle.py").read())
