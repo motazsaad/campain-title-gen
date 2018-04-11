59d54a03724fa930f34f5961b4d7bd65b53c9043
atefahmed <atefa2009@gmail.com>
2018-04-11 15:53:36 +0300

    version 1.15

---
+++ pytorch_char_level/helpers.py
@@ -0,0 +1,42 @@
+# https://github.com/spro/char-rnn.pytorch
+
+import unidecode
+import string
+import random
+import time
+import math
+import torch
+import re
+
+# Reading and un-unicode-encoding data
+
+all_characters = string.printable
+n_characters = len(all_characters)
+
+def read_file(filename):
+    file = unidecode.unidecode(open(filename,  encoding="utf-8").read())
+    file = file.lower()
+    # make normalization on text file
+    file = re.sub('[^A-Za-z0-9\n!"#$%&()*+,-./:;<=>?@[\]^_`{|}~\' ]+', '', file)
+    print(file)
+    return file, len(file)
+
+# Turning a string into a tensor
+
+def char_tensor(string):
+    tensor = torch.zeros(len(string)).long()
+    for c in range(len(string)):
+        try:
+            tensor[c] = all_characters.index(string[c])
+        except:
+            continue
+    return tensor
+
+# Readable time elapsed
+
+def time_since(since):
+    s = time.time() - since
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
---
+++ pytorch_char_level/test.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+#python train.py './description/train.txt'
+python train.py './description/train.txt' --cuda
