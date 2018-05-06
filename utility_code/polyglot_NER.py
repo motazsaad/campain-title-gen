974d36825d354df2e6fb9f28ee696c953061a83d
atefahmed <atefa2009@gmail.com>
2018-05-06 16:53:59 +0300

    version 1.20

---
+++ utility_code/polyglot_NER.py
@@ -0,0 +1,18 @@
+# from polyglot.downloader import downloader
+#
+# print(downloader.supported_languages_table("ner2", 3))
+
+from polyglot.text import Text
+
+blob = """The Israeli Prime Minister Benjamin Netanyahu has warned that Iran poses a "threat to the entire world"."""
+text = Text(blob)
+
+print(text)
+print(text.entities)
+# We can also specify language of that text by using
+# text = Text(blob, hint_language_code='en')
+
+# for sent in text.sentences:
+#   print(sent, "\n")
+#   for entity in sent.entities:
+#     print(entity.tag, entity)
