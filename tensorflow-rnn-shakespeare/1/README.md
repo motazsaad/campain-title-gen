1e97aa8b4525a0aa0b14f6fa4cbd016a56ba083f
atefahmed <atefa2009@gmail.com>
2018-08-26 15:38:04 +0300

    version 1.27

--- tensorflow-rnn-shakespeare/1/README.md
+++ tensorflow-rnn-shakespeare/1/README.md
@@ -9,7 +9,7 @@ library:
 Tensorflow
 
 Level:
-
+Char
 
 Model:
 RNN
--- utility_code/normalizationFile.py
+++ utility_code/normalizationFile.py
@@ -1,17 +1,18 @@
 import re
 
-input_file = 'NER/test.txt'
-# filter_file = 'output2.txt'
+input_file = 'dataset/shopzilla multiple domain/shopzillaUniqe.txt'
+filter_file = 'shopzillaUniqe_preprocesed.txt'
 
 with open(input_file, "r", encoding='utf-8') as f:
-    data = f.read()
+    data = f.readlines()
 
-data = data.lower()
+# data = data.lower()
 # make normalization on text file
 # data = re.sub('[^A-Za-z0-9\n!"#$%&()*+,-./:;<=>?@[\]^_`{|}~\' ]+', '', data)
 # print(data)
 
-data = re.sub('[^A-Za-z0-9!"#$%&()*+,-./:;<=>?@[\]^_`{|}~\' ]+', '', data)
-print(data)
-# with open(filter_file, "a") as myfile:
-#     myfile.write(data)
+for line in data:
+    new_line = re.sub('[^A-Za-z0-9!"#$%&()*+,-./:;<=>?@[\]^_`{|}~\' ]+', '', line.lower())
+    # print(data)
+    with open(filter_file, "a") as myfile:
+        myfile.write(new_line+'\n')
